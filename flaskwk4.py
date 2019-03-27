88bb8df1e6eaac0606fdf8cf3caa968412bce7ed
Jo Mehat <kirnjot.mehat@kcl.ac.uk>
2019-03-27 19:52:03 +0000

    fixing howitworks

--- flaskwk4.py
+++ flaskwk4.py
@@ -4,14 +4,14 @@ import requests
 app = Flask("MyApp")
 
 
-def send_simple_message(emailaddress):
-    return requests.post(
-        "https://api.mailgun.net/v3/sandbox2dead1799f1340f8b75a02d73e0a09d5.mailgun.org/messages",
-        auth=("api", "b6893791b11f1c3d588452ef3fea7fdc-de7062c6-b23a9e4e"),
-        data={"from": "Excited User <mailgun@sandbox2dead1799f1340f8b75a02d73e0a09d5.mailgun.org>",
-        "to": [emailaddress],
-        "subject": "Subscription successful",
-        "text": "Testing some Mailgun awesomeness!"})
+# def send_simple_message(emailaddress):
+#     return requests.post(
+#         "https://api.mailgun.net/v3/sandbox2dead1799f1340f8b75a02d73e0a09d5.mailgun.org/messages",
+#         auth=("api", "b6893791b11f1c3d588452ef3fea7fdc-de7062c6-b23a9e4e"),
+#         data={"from": "Excited User <mailgun@sandbox2dead1799f1340f8b75a02d73e0a09d5.mailgun.org>",
+#         "to": [emailaddress],
+#         "subject": "Subscription successful",
+#         "text": "Testing some Mailgun awesomeness!"})
 
 
 
@@ -24,17 +24,17 @@ def index():
 # def helloJo():
 #     return "Hello Jo"
 
-@app.route("/howitworks/")
-def hiw():
-    return render_template("howitworks.html")
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-@app.route("/<name>")
-def HelloStranger(name):
-    return render_template("index.html" , name = name )
+@app.route("/howitworks/")
+def hiw():
+    return render_template("howitworks.html")
+
+# @app.route("/<name>")
+# def HelloStranger(name):
+#     return render_template("index.html" , name = name )
 
 # @app.route("/signup", methods=["POST"])
 # def sign_up():
